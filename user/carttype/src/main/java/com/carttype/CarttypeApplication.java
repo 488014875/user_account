6f8916661d834fe681c994fa224cd8be32b176a9
488014875 <488014875@qq.com>
2019-04-07 17:46:21 +0800

    模板代码

--- user/carttype/src/main/java/com/carttype/CarttypeApplication.java
+++ user/carttype/src/main/java/com/carttype/CarttypeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarttypeApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(CarttypeApplication.class, args);
     }
 
--- user/carttype/src/main/java/com/carttype/controller/ICarttypeController.java
+++
@@ -1,8 +0,0 @@
-package com.carttype.controller;
-
-import java.util.Map;
-
-public interface ICarttypeController {
-
-    public String add();
-}
