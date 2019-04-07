aa7db4f09e67c9f046b197c94ecca250697bbee7
488014875 <488014875@qq.com>
2019-04-07 17:27:38 +0800

    模板代码

--- user/carttype/src/main/java/com/carttype/CarttypeApplication.java
+++ user/carttype/src/main/java/com/carttype/CarttypeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarttypeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CarttypeApplication.class, args);
     }
 
